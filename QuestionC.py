3d24af799ba305ca2ec561dcfef12a2a5bfe7517
Caelan Marks <caelan.marks@gmail.com>
2019-10-08 15:46:50 -0400

    Added geolocation to question c

--- QuestionC.py
+++ QuestionC.py
@@ -15,6 +15,7 @@
 # As a hint, we are not looking for quantity, but rather quality, maintainability, scalability,
 # testability and a code that you can be proud of.
 import time
+import os
 
 
 class LRU(object):
@@ -24,12 +25,19 @@ class LRU(object):
     region_list = []
     dataset = []
 
-    def __init__(self, region, num_slots=1, time_to_expiry=300):
+    def __init__(self, region, num_slots=1, time_to_expiry=300, region_list=[]):
         self.num_slots = num_slots
         self.time_to_expiry = time_to_expiry
         self.region = region
+        self.dataset = []
 
-    def write(self, data_id, data):
+        if not os.path.exists(self.region):
+            os.makedirs(self.region)
+
+        self.region_list = region_list
+        self.synchronize()
+
+    def write(self, data_id, data, local_region=True):
         # Remove expired cached data
         for i in range(len(self.dataset)):
             # Check if expired, if reached expired data, if reach data thats not expired, break cause rest of data
@@ -45,6 +53,14 @@ class LRU(object):
         else:
             del self.dataset[0]
             self.dataset.append([time.time(), data_id, data])
+
+        file = open(self.region + '/' + str(data_id) + '.txt', 'w')
+        file.write(str(data))
+
+        # Write to other regions
+        if local_region:
+            for region in self.region_list:
+                region.write(data_id, data, local_region=False)
 
     def read(self, data_id):
         # Look for data in local cache, starting at most recently used
@@ -66,21 +82,72 @@ class LRU(object):
 
                 return temp[2]
 
-        # TODO: Else search other regions
+        # Look for file in local region
+        # if os.path.exists(self.region + '/' + str(data_id) + '.txt'):
+        #     file = open(self.region + '/' + str(data_id) + '.txt', 'r')
+        #     data = file.read()
+        #
+        #     # Add to cache
+        #     if len(self.dataset) < self.num_slots:
+        #         self.dataset.append([time.time(), data_id, data])
+        #     else:
+        #         del self.dataset[0]
+        #         self.dataset.append([time.time(), data_id, data])
+        #
+        #     return data
 
-        pass
+        # Look in the databases of all the other regions
+        for region in self.region_list:
+            if os.path.exists(region.region + '/' + str(data_id) + '.txt'):
+                file = open(region.region + '/' + str(data_id) + '.txt', 'r')
+                data = file.read()
+
+                # Add to cache
+                if len(self.dataset) < self.num_slots:
+                    self.dataset.append([time.time(), data_id, data])
+                else:
+                    del self.dataset[0]
+                    self.dataset.append([time.time(), data_id, data])
+
+                return data
+
+        return print('File not found!')
 
     def add_new_region(self, region):
-        # TODO: Figure out how I want to connect regions such that they all know that each other exist
-        #  so that they can ensure data consistency across the regions
-        # LRU(region='North America', num_slots=self.num_slots, time_to_expiry=self.time_to_expiry)
-        pass
+        temp_region_list = self.region_list.copy()
+        temp_region_list.append(self)
+        new_region = LRU(region=region, num_slots=self.num_slots, time_to_expiry=self.time_to_expiry,
+                         region_list=temp_region_list)
+
+        for other_region in self.region_list.copy():
+            other_region.add_to_region_list(new_region)
+
+        self.region_list.append(new_region)
+
+        return new_region
+
+    def add_to_region_list(self, region):
+        self.region_list.append(region)
 
     def synchronize(self):
-        pass
+        for region in self.region_list:
+            for file_name in os.path(region.region): # TODO: Figure out how to do this
+                file = open(self.region.region + '/' + file_name, 'r')
+                data = file.read()
+            file.write(str(data))
 
+# TODO: After crash recovery, literally just del and reinstantiate with same name
 
 geo_lru = LRU(region='North America', num_slots=10, time_to_expiry=1)
+
+geo_lru.write(100, 0)
+
+eu_lru = geo_lru.add_new_region('Europe')
+
+la_lru = geo_lru.add_new_region('Latin America')
+
+af_lru = geo_lru.add_new_region('Africa')
+
 
 for j in range(0, 11):
     time.sleep(0.3)
